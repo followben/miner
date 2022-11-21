59598581d9580cec475d78f8ad19c96e73d0e1bf
Ben Stovold <bstovold@gmail.com>
2022-11-21 17:20:45 +1000

    Fix test formatting

--- main_test.py
+++ main_test.py
@@ -2,6 +2,7 @@ from main import schema
 
 from graphql import GraphQLError, SourceLocation
 
+
 def test_query_returns_sample_data():
     query = """
         query {
@@ -22,15 +23,16 @@ def test_query_returns_sample_data():
 
     assert result.errors is None
     assert result.data["person"] == {
-            "email": "a.miner@example.com",
-            "name": "Ashley Miner",
-            "address": {
-                "number": 99,
-                "street": "Hyalite St",
-                "city": "Sydney",
-                "state": "NSW",
-            },
-        }
+        "email": "a.miner@example.com",
+        "name": "Ashley Miner",
+        "address": {
+            "number": 99,
+            "street": "Hyalite St",
+            "city": "Sydney",
+            "state": "NSW",
+        },
+    }
+
 
 def test_schema_validates_incorrect_input():
     query = """
