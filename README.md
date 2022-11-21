b5954123c70f33a464e7d3b32609654466340187
Ben Stovold <bstovold@gmail.com>
2022-11-21 13:10:53 +1000

    Add initial files.

---
+++ README.md
@@ -0,0 +1,49 @@
+# miner
+
+A simple project demonstrating the use of [FastAPI](https://fastapi.tiangolo.com) and [Strawberry](https://strawberry.rocks) to handle a graphql query.
+
+## Quick Start
+
+Ensure python >=3.7+ is on your path, then:
+
+```bash
+% python -m venv venv
+% source venv/bin/activate
+(venv) % pip install --upgrade pip setuptools wheel
+(venv) % pip install -r requirements.txt
+(venv) % uvicorn main:app
+```
+
+Then either issue a standard POST query via your favourite HTTP cli:
+
+```bash
+curl -g \
+-X POST \
+-H "Content-Type: application/json" \
+-d '{"query":"query{person {email name address { number street city state }}}"}' \
+http://localhost:8000/graphql
+```
+
+Or, submit the same query via the browser at `http://localhost:8000/graphql`:
+
+```graphql
+query {
+  person {
+    email
+    name
+    address {
+      number
+      street
+      city
+      state
+    }
+  }
+}
+```
+
+## Related examples
+
+For more interesting examples, see:
+
+- [groundsim](https://github.com/followben/groundsim) - a more complex containerized example using FastAPI, Strawberry and Broadcaster that supports mutations, REST, a basic visualisation of the data in React, and deploys via CI/ CD to the cloud.
+- [datalogger](https://github.com/followben/datalogger) - another sample GraphQL api using Django and Graphene.
---
+++ main.py
@@ -0,0 +1,76 @@
+import enum
+
+import strawberry
+from fastapi import FastAPI
+from pydantic import BaseModel
+from strawberry.fastapi import GraphQLRouter
+
+app = FastAPI()
+
+
+# Pydantic types validate input
+
+
+class StateTerritory(str, enum.Enum):
+    NSW = "NSW"
+    VIC = "VIC"
+    QLD = "QLD"
+    NT = "NT"
+    ACT = "ACT"
+    TAS = "TAS"
+    WA = "WA"
+    SA = "SA"
+
+
+class Address(BaseModel):
+    number: int
+    street: str
+    city: str
+    state: StateTerritory
+
+
+class Person(BaseModel):
+    email: str
+    name: str
+    address: Address
+
+
+# Strawberry uses a code-first GraphQL schema can just bootstrap with pydantic
+
+# Register enums manually for now
+# https://github.com/strawberry-graphql/strawberry/issues/1598
+strawberry.enum(StateTerritory)
+
+
+@strawberry.experimental.pydantic.type(model=Address, all_fields=True)
+class AddressType:
+    pass
+
+
+@strawberry.experimental.pydantic.type(model=Person, all_fields=True)
+class PersonType:
+    pass
+
+
+# Define our Query type to support querying person returning sample data
+@strawberry.type
+class Query:
+    @strawberry.field
+    def person(self) -> PersonType:
+        sample = {
+            "email": "a.miner@example.com",
+            "name": "Ashley Miner",
+            "address": {
+                "number": 99,
+                "street": "Hylite St",
+                "city": "Sydney",
+                "state": "NSW",
+            },
+        }
+        return Person(**sample)
+
+
+# Create the schema and serve it via a FasrAPI endpoint
+schema = strawberry.Schema(Query)
+graphql_app = GraphQLRouter(schema)
+app.include_router(graphql_app, prefix="/graphql")
